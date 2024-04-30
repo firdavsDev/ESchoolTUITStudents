e1ed1815833f7c61a2f6e23edb424bcd683e0bbc
firdavsDev <xackercoder@gmail.com>
2024-05-01 03:26:13 +0500

    added home page

--- config/views.py
+++ config/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
 
+from events.models import Event
+from cource.models import Course
+
 
 def home(request):
     """A view that displays a main page."""
-    return render(request, 'index.html')
+    events = Event.objects.filter(is_active=True).order_by('-created')[:3]
+    courses = Course.objects.filter(is_active=True).order_by('-created')[:3]
+    context = {
+        'events': events,
+        'courses': courses
+    }
+    return render(request, 'index.html', context)
