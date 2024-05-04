c989bfdcc0399076b3021b0d0cd08854b3d62264
firdavsDev <xackercoder@gmail.com>
2024-05-04 22:44:45 +0500

    aded some changes

--- contact/signals.py
+++ contact/signals.py
@@ -9,7 +9,7 @@ from .models import Contact
 @receiver(post_save, sender=Contact)
 def create_contact(sender, instance, created, **kwargs):
     if created:
-        tg_alert = Alerter(bot_token='5799450453:AAHsE9kb4wSwttZwwPXWxmtkVzndTZTC64w',
-                           chat_id='-1001759379760')
+        tg_alert = Alerter(bot_token='6807947321:AAHoCa_ny3PdGOjKgm-v2s9MkcrMBDRIwv8',
+                           chat_id='-1002032242624')
         tg_alert.custom_alert(
             text=f'New contact: {instance.first_name} {instance.last_name} \n\nMessage: {instance.message}')
