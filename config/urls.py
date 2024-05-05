95bddc8f289ae23071685e86b94a8211b6e1c579
firdavsDev <xackercoder@gmail.com>
2024-05-06 02:31:03 +0500

    addef

--- config/urls.py
+++ config/urls.py
@@ -21,4 +21,7 @@ urlpatterns = [
     # contact app
     path('contact/', include('contact.urls')),
 
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
