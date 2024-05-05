d695bbfde1bbdc424495055d292fc72f36e3c190
firdavsDev <xackercoder@gmail.com>
2024-05-06 02:03:29 +0500

    aded somec

--- config/settings.py
+++ config/settings.py
@@ -115,6 +115,7 @@ STATICFILES_DIRS = [
     BASE_DIR / 'static'
 ]
 STATIC_ROOT = BASE_DIR / 'staticfiles'
+
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 # Default primary key field type
