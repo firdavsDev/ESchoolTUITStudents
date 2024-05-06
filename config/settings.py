ef02a65d699d88cb596a0254d607830670e5f387
firdavsDev <xackercoder@gmail.com>
2024-05-06 15:29:57 +0500

    added clints

--- config/settings.py
+++ config/settings.py
@@ -114,8 +114,12 @@ STATIC_URL = 'static/'
 # STATICFILES_DIRS = [
 #     BASE_DIR / 'static'
 # ]
+if DEBUG:
+    STATICFILES_DIRS = [
+        BASE_DIR / 'static'
+    ]
 
-STATIC_ROOT = BASE_DIR / 'static'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
